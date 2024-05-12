c5a6474d3283ddb2f4652da06aaca57c5fffe094
ninawagener <169111318+ninawagener@users.noreply.github.com>
2024-05-13 11:49:29 +1200

    Add files via upload

--- C_01_Instructions.py
+++ C_01_Instructions.py
@@ -1,19 +1,56 @@
-print("🎲🎲 Roll It 13! 🎲🎲")
+# checks users enter yes (y) or no (n)
+def yes_no(question):
+    while True:
+        response = input(question).lower()
 
+        # checks user response, question
+        # repeats if users don't enter yes / no
+        if response == "yes":
+           return "yes"
+        elif response == "y" :
+            return "yes"
+        elif response == "no":
+            return "no"
+        elif response == "n" :
+            return "no"
+        else:
+            print("Please enter yes / no")
+
+
+def instruction():
+    print('''
+    
+    **** Instructions ****
+    
+To begin, decide on a score goal (eg: The first one to get a 
+score of 50 wins).
+
+For each round of the game, you win points by rolling dice.
+The winner of the round is the one who gets 13 (or slightly less).
+
+If you win the round, then your score will increase by the
+number of points that you earned. If your first roll of two 
+dice is  double (eg: both dice show a three), then your score
+will be DOUBLE the number of points.
+
+If you lose the round, then you don't get any points.
+
+If you and the computer tie (eg: you both get a score of 11,
+then you will have 11 points added to your score
+    
+    ''')
+
+# Main routine
+print()
+print("🎲🎲 Roll It 13! 🎲🎲")
+print()
 
 # loop for testing purposes
 
-while True:
-    want_instructions = input("Do you want to read the instructions?") . lower()
+want_instructions = yes_no("Do you want to read the instructions?") . lower()
 
 # checks users enter yes (y) or no (n)
-    if want_instructions == "yes":
-        print ("you chose yes")
-    elif want_instructions == "y" :
-        print("you chose yes")
-    elif want_instructions == "no":
-        print("you chose no")
-    elif want_instructions == "n" :
-        print("you chose no")
-    else:
-        print("You did not choose a valid response")
+if want_instructions == "yes":
+   instruction()
+
+print("program")
