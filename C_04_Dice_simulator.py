54c8798e506e6ac80a0eaf0f7d72d9bff9372b55
ninawagener <169111318+ninawagener@users.noreply.github.com>
2024-05-20 11:41:18 +1200

    Double roll, checks for double points opportunity

--- C_04_Dice_simulator.py
+++ C_04_Dice_simulator.py
@@ -1,5 +1,28 @@
 import random
 
-for item in range (0, 10):
-    dice_roll = random.randint(1, 3)
-    print(dice_roll, end="\t")
+#generates an integer between 0 and 6
+# to simulate a roll of a die
+def roll_die() :
+    result = random.randint(1, 6)
+    return result
+
+#Main Rountine starts here
+
+for item in range(0, 10):
+    user_score = 0
+    double_score = "no"
+
+    #r oll two dice
+    roll_1 = roll_die()
+    roll_2 = roll_die()
+
+    # check if we have a double score opportunity
+    if roll_1 == roll_2:
+        double_score = "yes"
+
+    # Find the total points (so far)
+    user_points = roll_1 + roll_2
+
+    # Show the user the result
+    print(f"Die 1: {roll_1} \t Die 2: {roll_2} \t Points: {user_points}")
+    print(f"Double score oppotunity: {double_score}")
